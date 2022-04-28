4c08d8fbc16314d85b7085a50bd60ba122a35ff1
joão marcelo daltro marques <joaomarques@divtech.com.br>
2022-04-28 19:12:09 -0300

    Ajustado layout

--- Lib.Data/Entities/ProjetoDal.cs
+++ Lib.Data/Entities/ProjetoDal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace Lib.Data
 {
     public class ProjetoDal : BaseDal
     {
+
+        [MaxLengthAttribute(27)]
         public string Nome { get; set; }
         public string Desc { get; set; }
         public int Prioridade { get; set; }
